20511107495717ba4404f0fb720b459d2de5ce87
Emanuel Zabaleta <emanuelzm0220@gmail.com>
2022-12-05 14:03:03 -0500

    update structure

---
+++ pages/blog/[id].jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import CustomHead from "../../components/Custom-Head"
+
+const blogItem = (id) => {
+    return (
+        <div>
+            <CustomHead title={"Blog"}></CustomHead>
+            <h1>Blog</h1>
+        </div>
+    )
+}
---
+++ pages/blog/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import CustomHead from "../../components/Custom-Head"
+
+const blog = () => {
+    return (
+        <div>
+            <CustomHead title={"Blog"}></CustomHead>
+            <h1>Blog</h1>
+        </div>
+    )
+}
+
+export default blog
